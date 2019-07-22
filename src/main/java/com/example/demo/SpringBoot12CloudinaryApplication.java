422f56f56db0e5b365094bd317e46b6d366a34ea
GBTC441007ur <samduru1@gmail.com>
2019-07-22 14:06:19 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot12CloudinaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot12CloudinaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot12CloudinaryApplication.class, args);
+    }
+
+}
